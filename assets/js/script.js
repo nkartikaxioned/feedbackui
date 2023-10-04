047b7392f16c198137690e33849fc417757c443f
nkartikaxioned <kartikn@axioned.com>
2023-10-04 16:01:17 +0530

    js add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,41 @@
 
 */
 
+const feedcontainer=document.querySelector('.feedback_container');
+const wrapcontainer=document.querySelector('.wrapper');
+const feedback=document.querySelectorAll('.rating');
+const button=document.querySelector('.button');
+const texts=document.querySelectorAll('.text');
+let stringFeedback="";
 
 
+feedback.forEach((element,index) => {
+     element.addEventListener('click',() => {
+      removActive();
+        element.classList.add('active');
+        stringFeedback=(texts[index].innerHTML).toUpperCase();
+        
+        console.log(stringFeedback)
+     })
+        
+})
 
+function removActive(){
+    feedback.forEach(elem => {
+        elem.classList.remove('active');
+    })
+}
 
+button.addEventListener('click',() => {
+    wrapcontainer.innerHTML=`
+    <p>&#9829;</p>
+    <strong>Thank You!</strong>
+    <br>
+    Feedback: ${stringFeedback}
+    <br><br>
+    <p>We'll use your feedback to improve our customer support</p>
+    `
+})
 
 
 
